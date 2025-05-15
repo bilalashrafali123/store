de327cb822ad1cf5bf3a81d5299d4fa90140eac4
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-15 18:34:00 +0500

    updated...

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,28 @@ function App() {
           <div className="nav-logo">
             <h2>KARMA</h2>
           </div>
-
-          
-          <div className="hamburger"><h1><i className="fa-solid fa-bars"></i></h1></div>
-
-          {/* Nav links */}
+          <div className="hamburger">
+            <h1>
+              <i className="fa-solid fa-bars"></i>
+            </h1>
+          </div>
           <span className="nav-links">
             <a href="#">Home</a>
             <a href="#">Shop</a>
             <a href="#">Blog</a>
             <a href="#">Pages</a>
             <a href="#">Contact</a>
-            <a href="#"><i className="fa-solid fa-bag-shopping"></i></a>
-            <a href="#"><i className="fa-solid fa-magnifying-glass"></i></a>
+            <a href="#">
+              <i className="fa-solid fa-bag-shopping"></i>
+            </a>
+            <a href="#">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </a>
           </span>
         </nav>
       </div>
 
-    
-      <div className="store-background">
-        <h1>Home</h1>
-      </div>
+      <div className="store-background"></div>
     </>
   );
 }
