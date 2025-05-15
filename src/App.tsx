fac6b3b50fbbb31620f91a3e0213c2e3ffb84804
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-15 22:19:21 +0500

    card-section completed

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
       </div>
       <div className="card-section">
         <div className="card" style={{ width: "20rem" }}>
-          <img src={"/src/p4.jpg"} className="card-img-top" alt="..." />
+          <img src={"/src/p1.jpg"} className="card-img-top" alt="..." />
           <div className="card-body"></div>
           <p className="card-text">
             Addidas New Hammer sole <br /> for sports person
@@ -66,41 +66,13 @@ function App() {
         </div>
 
         <div className="card" style={{ width: "20rem" }}>
-          <img src={"/src/p1.jpg"} className="card-img-top" alt="..." />
-          <div className="card-body"></div>
-          <p className="card-text">
-            Addidas New Hammer sole <br /> for sports person
-          </p>
-          <p className="money">
-            $150{" "}
-            <span>
-              <del>$210</del>
-            </span>
-          </p>
-          <div className="icons">
-            <span>
-              <i className="fa-solid fa-cart-shopping"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-heart"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-bookmark"></i>
-            </span>
-            <span>
-              <i className="fa-solid fa-thumbs-up"></i>
-            </span>
-          </div>
-        </div>
-
-        <div className="card" style={{ width: "20rem" }}>
           <img src={"/src/p2.jpg"} className="card-img-top" alt="..." />
           <div className="card-body"></div>
           <p className="card-text">
             Addidas New Hammer sole <br /> for sports person
           </p>
           <p className="money">
-            $150{" "}
+            $150
             <span>
               <del>$210</del>
             </span>
@@ -120,9 +92,143 @@ function App() {
             </span>
           </div>
         </div>
-
         <div className="card" style={{ width: "20rem" }}>
           <img src={"/src/p3.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p4.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p5.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p6.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p7.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+       <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p8.jpg"} className="card-img-top" alt="..." />
           <div className="card-body"></div>
           <p className="card-text">
             Addidas New Hammer sole <br /> for sports person
