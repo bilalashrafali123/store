f1c35af27709eed08076d9da9f5b9feb8c7155ec
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-15 20:54:09 +0500

    still  working...

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,127 @@ function App() {
           </span>
         </nav>
       </div>
-
       <div className="store-background"></div>
+      <div className="latest-products">
+        <h1>Latest Products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, <br /> adipisicing elit.
+          Delectus, animi.
+        </p>
+      </div>
+      <div className="card-section">
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p4.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p1.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150{" "}
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p2.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150{" "}
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "20rem" }}>
+          <img src={"/src/p3.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body"></div>
+          <p className="card-text">
+            Addidas New Hammer sole <br /> for sports person
+          </p>
+          <p className="money">
+            $150
+            <span>
+              <del>$210</del>
+            </span>
+          </p>
+          <div className="icons">
+            <span>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-heart"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-bookmark"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-thumbs-up"></i>
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
