677f7bc031500593bad35af780581b241740d668
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-15 22:58:15 +0500

    completed...

--- src/App.tsx
+++ src/App.tsx
@@ -227,7 +227,7 @@ function App() {
             </span>
           </div>
         </div>
-       <div className="card" style={{ width: "20rem" }}>
+        <div className="card" style={{ width: "20rem" }}>
           <img src={"/src/p8.jpg"} className="card-img-top" alt="..." />
           <div className="card-body"></div>
           <p className="card-text">
@@ -255,6 +255,54 @@ function App() {
           </div>
         </div>
       </div>
+      <footer>
+        <div className="footer-container">
+          <div className="footer-box about">
+            <h3>About Us</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore dolore magna aliqua.
+            </p>
+          </div>
+         <div className="footer-box newsletter">
+            <h3>Newsletter</h3>
+            <p>Stay update with our latest</p>
+            <div className="newsletter-input">
+              <input type="email" placeholder="Enter Email" />
+              <button>→</button>
+            </div>
+          </div>
+          <div className="footer-box instagram">
+            <h3>Instagram Feed</h3>
+            <div className="insta-images">
+              <img src={"/src/i1.jpg"} alt="" />
+              <img src={"/src/i2.jpg"} alt="" />
+              <img src={"/src/i3.jpg"} alt="" />
+              <img src={"/src/i4.jpg"} alt="" />
+              <img src={"/src/i5.jpg"} alt="" />
+              <img src={"/src/i6.jpg"} alt="" />
+              <img src={"/src/i7.jpg"} alt="" />
+              <img src={"/src/i8.jpg"} alt="" />
+            </div>
+          </div>
+          <div className="footer-box follow">
+            <h3>Follow Us</h3>
+            <p>Let us be social</p>
+            <div className="social-icons">
+              <a href="#"><i className="fa-brands fa-facebook"></i></a>
+              <a href="#"><i className="fa-brands fa-twitter"></i></a>
+              <a href="#"><i className="fa-brands fa-github"></i></a>
+              <a href="#"><i className="fa-solid fa-bell"></i></a>
+            </div>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>
+            Copyright ©2025 All rights reserved | This template is made with{" "}
+            <span>♡</span> by <a href="#">Colorlib</a>
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
