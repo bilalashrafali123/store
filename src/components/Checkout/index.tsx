5d182ddc3d75526b795e1fc510d2272d00c9a99d
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-20 00:08:13 +0500

    completed...

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -14,63 +14,70 @@ const Cart = () => {
           </p>
 
           <div className="info-section">
-            <table className="info-table">
-              <thead>
-                <tr>
-                  <th>Order Info</th>
-                  <th>Billing Address</th>
-                  <th>Shipping Address</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <strong>Order number</strong>:6002
-                  </td>
-                  <td>
-                    <strong>Street</strong>: 56/8
-                  </td>
-                  <td>
-                    <strong>Street</strong>: 56/8
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Date</strong>: Los Angeles
-                  </td>
-                  <td>
-                    <strong>City</strong>: Los Angeles
-                  </td>
-                  <td>
-                    <strong>City</strong>: Los Angeles
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Total</strong>: USD 2210
-                  </td>
-                  <td>
-                    <strong>Country</strong>: United States
-                  </td>
-                  <td>
-                    <strong>Country</strong>: United States
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Payment method</strong>: Check payments
-                  </td>
-                  <td>
-                    <strong>Postcode</strong>: 36952
-                  </td>
-                  <td>
-                    <strong>Postcode</strong>: 36952
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="info-table-wrapper">
+              <h4>Order Info</h4>
+              <table className="info-table">
+                <tbody>
+                  <tr>
+                    <td><strong>Order number</strong>: 6002</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Date</strong>: Los Angeles</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Total</strong>: USD 2210</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Payment method</strong>: Check payments</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+        
+            <div className="info-table-wrapper">
+              <h4>Billing Address</h4>
+              <table className="info-table">
+                <tbody>
+                  <tr>
+                    <td><strong>Street</strong>: 56/8</td>
+                  </tr>
+                  <tr>
+                    <td><strong>City</strong>: Los Angeles</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Country</strong>: United States</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Postcode</strong>: 36952</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+           
+            <div className="info-table-wrapper">
+              <h4>Shipping Address</h4>
+              <table className="info-table">
+                <tbody>
+                  <tr>
+                    <td><strong>Street</strong>: 56/8</td>
+                  </tr>
+                  <tr>
+                    <td><strong>City</strong>: Los Angeles</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Country</strong>: United States</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Postcode</strong>: 36952</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
 
+        
           <div className="order-details">
             <h4>Order Details</h4>
             <table>
@@ -98,27 +105,19 @@ const Cart = () => {
                   <td>$720.00</td>
                 </tr>
                 <tr>
-                  <td>
-                    <strong>SUBTOTAL</strong>
-                  </td>
+                  <td><strong>SUBTOTAL</strong></td>
                   <td></td>
                   <td>$2160.00</td>
                 </tr>
                 <tr>
-                  <td>
-                    <strong>SHIPPING</strong>
-                  </td>
+                  <td><strong>SHIPPING</strong></td>
                   <td></td>
                   <td>Flat rate: $50.00</td>
                 </tr>
                 <tr>
-                  <td>
-                    <strong>TOTAL</strong>
-                  </td>
+                  <td><strong>TOTAL</strong></td>
                   <td></td>
-                  <td>
-                    <strong>$2210.00</strong>
-                  </td>
+                  <td><strong>$2210.00</strong></td>
                 </tr>
               </tbody>
             </table>
--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import Banner from "../Banner";
 import "./index.scss";
 
+const tabs = {
+  description: "description",
+  specification: "specification",
+};
+
 const Detail = () => {
+  const [Tab, setTab] = useState(tabs.description);
+
   return (
     <div>
       <Banner title="Details" />
@@ -28,41 +36,99 @@ const Detail = () => {
 
             <div className="product-action">
               <div className="quantity-group">
-                <label>Quantity: {""}</label>
-                <input type="number"  value={1}/>
+                <label>Quantity: </label>
+                <input type="number" defaultValue={1} />
               </div>
               <button>Add to Cart</button>
             </div>
           </div>
         </div>
+
         <div className="product-tabs">
-          <button className="active">Description</button>
-          <button>Specification</button>
+          <button
+            className={Tab === tabs.description ? "active" : ""}
+            onClick={() => setTab(tabs.description)}
+          >
+            Description
+          </button>
+          <button
+            className={Tab === tabs.specification ? "active" : ""}
+            onClick={() => setTab(tabs.specification)}
+          >
+            Specification
+          </button>
         </div>
-        <div className="product-info">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially
-            unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently
-            with desktop publishing software like Aldus PageMaker including
-            versions of Lorem Ipsum.
-          </p>
-        </div>
+
+        {Tab === tabs.description && (
+          <div className="product-info">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It
+              has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum
+            </p>
+          </div>
+        )}
+
+        {Tab === tabs.specification && (
+          <div className="product-specs">
+            <table>
+              <tbody>
+                <tr>
+                  <th>witdh</th>
+                  <td>128mm</td>
+                </tr>
+                <tr>
+                  <th>height</th>
+                  <td>508</td>
+                </tr>
+                <tr>
+                  <th>depth</th>
+                  <td>85mm</td>
+                </tr>
+                <tr>
+                  <th>weight</th>
+                  <td>52gm</td>
+                </tr>
+                <tr>
+                  <th>quality checking</th>
+                  <td>yes</td>
+                </tr>
+                <tr>
+                  <th>Freshness Duration</th>
+                  <td>03 days</td>
+                </tr>
+                <tr>
+                  <th>When packeting</th>
+                  <td>without touching end</td>
+                </tr>
+                <tr>
+                  <th>Each box Contains </th>
+                  <td>60pcs</td>
+                </tr>
+                
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
   );
 };
+
 export default Detail;
